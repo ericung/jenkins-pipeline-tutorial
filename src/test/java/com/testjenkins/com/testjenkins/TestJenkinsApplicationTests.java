41d87a0ba0993130fa9643df63e713effdb47170
Eric Ung <ungericw@tutanota.com>
2020-08-07 12:50:20 -0500

    first commit

---
+++ src/test/java/com/testjenkins/com/testjenkins/TestJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testjenkins.com.testjenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestJenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
